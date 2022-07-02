e7435e89173b595924a927c65ed5d7a6962ff9db
MGuyF <2000291gf@gmail.com>
2022-07-02 05:12:00 -0700

    New changes

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *  
+ *
+ * Print: Hello world
+ *
  * Return: Always 0 (Success)
  */
 int main (void)
 {
+	printf("Hello world\n");
 	return (0);
 }
