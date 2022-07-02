28e5aaad1f0ef61f4fbf805323a2b2a962500267
MGuyF <2000291gf@gmail.com>
2022-07-02 04:17:24 -0700

    Task2 0x00_hello-C,Hello,World project

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- *Print: Hello,World 
+ *  
  * Return: Always 0 (Success)
  */
 int main (void)
 {
-	printf("Hello,World\n");
 	return (0);
 }
