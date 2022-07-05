a016175fe0fca3de6e3fc374c5aa29b066f6ed2c
MGuyF <2000291gf@gmail.com>
2022-07-05 14:01:25 -0700

    Tasks 0x02-functions_nested_loops

---
+++ 0x00-hello_world/4-1puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * Main entry point
+ *
+ * Char type
+ *
+ * Puts() function
+ *
+ * Return (always 0)
+ */
+int main(void)/* Main function returns 0 */
+{
+		puts("\"Programming is like building a multilingual puzzle\n");
+		return (0);
+}
